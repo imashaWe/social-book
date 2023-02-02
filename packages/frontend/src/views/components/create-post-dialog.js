87e43d1da5a442978495af3c23627d74ec018a19
imasshaWe <imasha98.we@gmail.com>
2023-02-02 22:33:59 +0530

    add post upload

---
+++ packages/frontend/src/views/components/create-post-dialog.js
@@ -0,0 +1,40 @@
+import {
+    Button,
+    Dialog,
+    DialogActions,
+    DialogContent,
+    DialogTitle,
+    TextField
+} from "@mui/material";
+import ImageSelector from "./image-selector";
+
+export default function CreatePostDialog({open, handleClose, handleSubmittingPost}) {
+    let imageFile, description;
+    const handlePost = () => {
+        if (imageFile) {
+            handleSubmittingPost(imageFile, description);
+            handleClose();
+        }
+    }
+
+    return (
+        <Dialog open={open} onClose={handleClose} fullWidth={true}>
+            <DialogTitle>Create a new post</DialogTitle>
+            <DialogContent>
+                <ImageSelector handleImageChange={(image) => imageFile = image}/>
+                <TextField
+                    margin="dense"
+                    label="Description"
+                    multiline
+                    rows={4}
+                    fullWidth
+                    onChange={(event) => description = event.target.value}
+                />
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={handleClose}>Cancel</Button>
+                <Button onClick={handlePost} variant='contained'>POST</Button>
+            </DialogActions>
+        </Dialog>
+    );
+}
--- packages/frontend/src/views/pages/home.js
+++ packages/frontend/src/views/pages/home.js
@@ -1,8 +1,22 @@
 import {Box, Fab, Paper, Stack} from "@mui/material";
 import Post from "../components/post";
 import AddIcon from '@mui/icons-material/Add';
+import {useState} from "react";
+import CreatePostDialog from "../components/create-post-dialog";
 
 export default function Home() {
+    const [open, setOpen] = useState(false);
+
+    const handleClose = () => {
+        setOpen(false);
+    }
+    const handleOpen = () => {
+        setOpen(true);
+    }
+
+    const handleSubmittingPost = (imageFile, description) => {
+
+    }
 
     const postListing = Array.from(new Array(10)).map((post, index) => <Post key={index} post={post}/>);
 
@@ -28,10 +42,12 @@ export default function Home() {
                      bottom: 20,
                      left: 'auto',
                      position: 'fixed',
-                 }}>
+                 }}
+                 onClick={handleOpen}>
                 <AddIcon sx={{mr: 1}}/>
                 Add Post
             </Fab>
+            <CreatePostDialog handleClose={handleClose} handleSubmittingPost={handleSubmittingPost} open={open}/>
         </Paper>
     );
 }
