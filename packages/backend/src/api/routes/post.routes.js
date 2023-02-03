543c9021a989e258f9395935a7597ef348be67a7
imasshaWe <imasha98.we@gmail.com>
2023-02-04 01:16:47 +0530

    add like and unlike functions

--- packages/backend/src/api/routes/post.routes.js
+++ packages/backend/src/api/routes/post.routes.js
@@ -7,6 +7,8 @@ const upload = multer({dest: 'uploads/'});
 
 router.post('/', upload.single('image'), postController.addPost);
 router.get('/', postController.fetchPosts);
+router.post('/:postID/like', postController.likePost);
+router.post('/:postID/unlike', postController.unlikePost);
 
 module.exports = router;
 
