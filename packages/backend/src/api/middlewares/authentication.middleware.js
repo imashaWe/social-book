a1dc76669081568b7c983419cf3c3af4ec0958c7
imasshaWe <imasha98.we@gmail.com>
2023-02-03 15:41:52 +0530

    add auth middleware

---
+++ packages/backend/src/api/middlewares/authentication.middleware.js
@@ -0,0 +1,11 @@
+const {expressjwt: jwt} = require("express-jwt");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const authentication = jwt({
+    secret: process.env.ACCESS_TOKEN_SECRET,
+    algorithms: ["HS256"],
+}).unless({path: ["/auth/login", '/auth/register']});
+
+module.exports = authentication;
--- packages/backend/src/server.js
+++ packages/backend/src/server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongodb = require('./database/mongodb');
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const authentication = require("./api/middlewares/authentication.middleware");
 
 const app = express();
 
@@ -15,10 +16,12 @@ mongodb.connect();
 
 // middlewares
 app.use(cors());
+app.use(authentication);
 app.use(bodyParser.json());
 
 // routes
 app.use('/auth', require('./api/routes/auth.routes'));
+app.use('/test', require('./api/routes/test.routes'));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
