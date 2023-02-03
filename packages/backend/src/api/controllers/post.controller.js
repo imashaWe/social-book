25eb6b2bb2f0a122dda6791f74f3fd83e2e1b40b
imasshaWe <imasha98.we@gmail.com>
2023-02-04 01:11:20 +0530

    add like and unlike functions to post controller

--- packages/backend/src/api/controllers/post.controller.js
+++ packages/backend/src/api/controllers/post.controller.js
@@ -23,4 +23,29 @@ const fetchPosts = (req, res) => {
         });
 }
 
-module.exports = {addPost, fetchPosts}
+const likePost = (req, res) => {
+    const {uid} = req.auth;
+    const {postID} = req.params;
+
+    postService.likePost(postID, uid)
+        .then(() => {
+            res.status(200).json({message: 'Post successfully liked'});
+        })
+        .catch(err => {
+            res.status(200).json({message: err.message});
+        });
+}
+
+const unlikePost = (req, res) => {
+    const {uid} = req.auth;
+    const {postID} = req.params;
+    postService.unlikePost(postID, uid)
+        .then(() => {
+            res.status(200).json({message: 'Post successfully unliked'});
+        })
+        .catch(err => {
+            res.status(200).json({message: err.message});
+        });
+}
+
+module.exports = {addPost, fetchPosts, likePost, unlikePost}
