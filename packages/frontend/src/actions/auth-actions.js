c00ac1b9f6cc146a23ea1f4ef6e85a580aa82f80
imasshaWe <imasha98.we@gmail.com>
2023-02-04 01:48:41 +0530

    add login action

--- packages/frontend/src/actions/auth-actions.js
+++ packages/frontend/src/actions/auth-actions.js
@@ -1,18 +1,9 @@
 import {setFormSubmitting, setFormSuccess} from "./form-actions";
 
-export const login = (data, signIn) => {
+export const login = (data) => {
     return (dispatch) => {
         dispatch(setFormSubmitting());
-        setTimeout(() => {
-            signIn(
-                {
-                    user: data,
-                    token: '888888',
-                    expiresIn:'4000',
-                    tokenType: "Bearer", // Only if you are using refreshToken feature
-                });
-            dispatch(setFormSuccess())
-        }, 3000)
+      
 
     };
 }
--- packages/frontend/src/actions/user-action.js
+++ packages/frontend/src/actions/user-action.js
@@ -1,10 +1,27 @@
 import {SET_USER_LOGIN, SET_USER_LOGOUT} from "./types";
+import {setFormFailure, setFormSubmitting, setFormSuccess} from "./form-actions";
+import axios from "axios";
 
 export const setUserLogin = (user) => {
-    return {
-        type: SET_USER_LOGIN,
-        payload: user
-    };
+    return (dispatch) => {
+        dispatch(setFormSubmitting());
+        axios.post('auth/login', user)
+            .then((response) => {
+                if (response.data.status) {
+                    dispatch(setFormSuccess(response.data.message));
+                    dispatch({
+                        type: SET_USER_LOGIN,
+                        payload: response.data.user
+                    });
+                } else {
+                    dispatch(setFormFailure(response.data.message));
+                }
+            })
+            .catch((error) => {
+                dispatch(setFormFailure(error.message));
+            });
+    }
+
 }
 
 export const setUserLogout = () => {
