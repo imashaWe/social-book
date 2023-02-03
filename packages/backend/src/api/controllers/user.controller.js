ed19760947f47b44f087f83881267d09764ce1be
imasshaWe <imasha98.we@gmail.com>
2023-02-04 01:33:48 +0530

    update user data

--- packages/backend/src/api/controllers/user.controller.js
+++ packages/backend/src/api/controllers/user.controller.js
@@ -4,7 +4,7 @@ const register = (req, res) => {
     userServices
         .createUser(firstName, lastName, email, password)
         .then((data) =>
-            res.status(200).json({...data, message: 'User successfully registered'}))
+            res.status(200).json({message: 'User successfully registered', user: data}))
         .catch((e) => res.status(200).json({message: e.message}));
 }
 
@@ -12,7 +12,7 @@ const login = (req, res) => {
     const {email, password} = req.body;
     userServices
         .loginUser(email, password).then((data) =>
-        res.status(200).json({...data, message: 'User successfully logged in'}))
+        res.status(200).json({message: 'User successfully logged in', user: data}))
         .catch((e) => res.status(200).json({message: e.message}));
 }
 
