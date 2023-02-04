a31911b6928777de98e01d4a98c12aed992f55e6
imasshaWe <imasha98.we@gmail.com>
2023-02-04 13:15:54 +0530

    add update post reducer

--- packages/frontend/src/reducers/post-reducer.js
+++ packages/frontend/src/reducers/post-reducer.js
@@ -1,11 +1,15 @@
-import {SET_POSTS, UPDATE_POSTS} from "../actions/types";
+import {SET_POSTS, UPDATE_POST} from "../actions/types";
 
 export default function postReducer(state = [], action) {
     switch (action.type) {
         case SET_POSTS:
             return action.payload;
-        case UPDATE_POSTS:
-
+        case UPDATE_POST:
+            const index = state.findIndex(post => post._id === action.payload._id);
+            if (index !== -1) {
+                state[index] = action.payload;
+            }
+            return [...state];
         default:
             return state;
     }
