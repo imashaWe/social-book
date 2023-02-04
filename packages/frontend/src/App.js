11c7ff6ed9baca93492e50fc41dd2c4a99c0c609
imasshaWe <imasha98.we@gmail.com>
2023-02-04 11:29:11 +0530

    add Authorization header to axios

--- packages/frontend/src/App.js
+++ packages/frontend/src/App.js
@@ -1,7 +1,16 @@
 import {useRoutes} from "react-router-dom";
 import routes from "./config/routes";
+import {useSelector} from "react-redux";
+import {useEffect} from "react";
+import axios from "axios";
 
 export default function App() {
-    const router = useRoutes(routes);
-    return router;
+    const user = useSelector(state => state.user);
+
+    useEffect(() => {
+        axios.defaults.headers.common['Authorization'] = `Bearer ${user.token}`;
+    }, [user]);
+
+    return useRoutes(routes);
+
 }
