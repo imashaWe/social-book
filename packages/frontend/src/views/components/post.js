f39ac15996a63884e72cbbb9c090fc542814557a
imasshaWe <imasha98.we@gmail.com>
2023-02-04 13:57:00 +0530

    fix like issues

--- packages/frontend/src/views/components/post.js
+++ packages/frontend/src/views/components/post.js
@@ -9,13 +9,28 @@ import {
     Typography
 } from "@mui/material";
 import FavoriteIcon from '@mui/icons-material/FavoriteOutlined';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShareIcon from '@mui/icons-material/Share';
 import ProfileAvatar from "../common/profile-avatar";
 import nameToTitle from "../../helpers/name-to-title";
 import Moment from "react-moment";
+import {useDispatch, useSelector} from "react-redux";
+import {likePost, unlikePost} from "../../actions/post-action";
 
 export default function Post({post}) {
+    const dispatch = useDispatch();
     const fullName = nameToTitle(`${post.postedBy.firstName} ${post.postedBy.lastName}`);
+    const {uid} = useSelector(state => state.user);
+    const isLiked = post.likes.findIndex(like=>like._id === uid) !== -1;
+    const likeCount = post.likes.length;
+    const handleLike = () => {
+        if (isLiked) {
+            dispatch(unlikePost(post._id));
+        } else {
+            dispatch(likePost(post._id));
+        }
+    }
+
     return (
         <Card>
             <CardHeader
@@ -40,9 +55,10 @@ export default function Post({post}) {
             <CardActions>
                 <Grid container justifyContent="space-between">
                     <Grid item>
-                        <IconButton aria-label="add to favorites">
-                            <FavoriteIcon color="primary"/>
-                            <Typography variant="body2" color="text.secondary">12</Typography>
+                        <IconButton aria-label="add to favorites" onClick={handleLike}>
+                            {isLiked ? <FavoriteIcon color="primary"/> : <FavoriteBorderIcon/>}
+                            {likeCount > 0 &&
+                                <Typography variant="body2" color="text.secondary">{likeCount}</Typography>}
                         </IconButton>
                     </Grid>
                     <Grid item>
