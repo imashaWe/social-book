0d16ac15a0204eacb80d6f54277ab6b8c1b76656
imasshaWe <imasha98.we@gmail.com>
2023-02-04 18:25:31 +0530

    add post option menu to posts

--- packages/frontend/src/views/components/post.js
+++ packages/frontend/src/views/components/post.js
@@ -16,13 +16,26 @@ import nameToTitle from "../../helpers/name-to-title";
 import Moment from "react-moment";
 import {useDispatch, useSelector} from "react-redux";
 import {likePost, unlikePost} from "../../actions/post-action";
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import {useState} from "react";
+import PostOptionMenu from "./post-option-menu";
 
-export default function Post({post}) {
+export default function Post({post, handleEdit, handleDelete}) {
     const dispatch = useDispatch();
     const fullName = nameToTitle(`${post.postedBy.firstName} ${post.postedBy.lastName}`);
     const {uid} = useSelector(state => state.user);
     const isLiked = post.likes.findIndex(like => like._id === uid) !== -1;
     const likeCount = post.likes.length;
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const handleOptionMenuOpen = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleOptionMenuClose = () => {
+        setAnchorEl(null);
+    };
+
     const handleLike = () => {
         if (isLiked) {
             dispatch(unlikePost(post._id));
@@ -32,44 +45,52 @@ export default function Post({post}) {
     }
 
     return (
-        <Card elevation={5}>
-            <CardHeader
-                avatar={
-                    <ProfileAvatar user={post.postedBy}/>
-                }
+        <>
+            <Card elevation={5}>
+                <CardHeader
+                    avatar={
+                        <ProfileAvatar user={post.postedBy}/>
+                    }
 
-                title={fullName}
-                subheader={<Moment fromNow>{post.postedAt}</Moment>}
-            />
-            <CardMedia
-                component="img"
-                height="400"
-                image={post.imageURL}
-                alt="Post Image"
-            />
-
-            <CardContent>
-                <Typography variant="body2" color="text.secondary">
-                    {post.description !== "undefined" ? post.description : "   "}
-                </Typography>
-            </CardContent>
-
-            <CardActions>
-                <Grid container justifyContent="space-between">
-                    <Grid item>
-                        <IconButton aria-label="add to favorites" onClick={handleLike}>
-                            {isLiked ? <FavoriteIcon color="primary"/> : <FavoriteBorderIcon/>}
-                            {likeCount > 0 &&
-                                <Typography variant="body2" color="text.secondary">{likeCount}</Typography>}
+                    title={fullName}
+                    subheader={<Moment fromNow>{post.postedAt}</Moment>}
+                    action={
+                        <IconButton aria-label="settings" onClick={handleOptionMenuOpen}>
+                            <MoreVertIcon/>
                         </IconButton>
+                    }
+                />
+                <CardMedia
+                    component="img"
+                    height="400"
+                    image={post.imageURL}
+                    alt="Post Image"
+                />
+
+                <CardContent>
+                    <Typography variant="body2" color="text.secondary">
+                        {post.description !== "undefined" ? post.description : "   "}
+                    </Typography>
+                </CardContent>
+
+                <CardActions>
+                    <Grid container justifyContent="space-between">
+                        <Grid item>
+                            <IconButton aria-label="add to favorites" onClick={handleLike}>
+                                {isLiked ? <FavoriteIcon color="primary"/> : <FavoriteBorderIcon/>}
+                                {likeCount > 0 &&
+                                    <Typography variant="body2" color="text.secondary">{likeCount}</Typography>}
+                            </IconButton>
+                        </Grid>
+                        <Grid item>
+                            <IconButton aria-label="share">
+                                <ShareIcon/>
+                            </IconButton>
+                        </Grid>
                     </Grid>
-                    <Grid item>
-                        <IconButton aria-label="share">
-                            <ShareIcon/>
-                        </IconButton>
-                    </Grid>
-                </Grid>
-            </CardActions>
-        </Card>
+                </CardActions>
+            </Card>
+            <PostOptionMenu anchorEl={anchorEl} isAdmin={true} handleClose={handleOptionMenuClose}/>
+        </>
     );
 }
