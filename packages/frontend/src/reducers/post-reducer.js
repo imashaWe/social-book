c27c349210d1e33bee048dbabd68b2dfaa8860a8
imasshaWe <imasha98.we@gmail.com>
2023-02-04 11:52:21 +0530

    add post reducer

---
+++ packages/frontend/src/reducers/post-reducer.js
@@ -0,0 +1,11 @@
+import {SET_POSTS} from "../actions/types";
+
+export default function postReducer(state = [], action) {
+    switch (action.type) {
+        case SET_POSTS:
+            return action.payload;
+        default:
+            return state;
+    }
+}
+
