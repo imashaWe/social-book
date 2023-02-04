deded1e882240dbe97366cba4fccc8f08b3de244
imasshaWe <imasha98.we@gmail.com>
2023-02-04 11:53:23 +0530

    add post reducer

--- packages/frontend/src/reducers/index.js
+++ packages/frontend/src/reducers/index.js
@@ -2,10 +2,12 @@ import {combineReducers} from 'redux';
 import formReducer from "./form-reducer";
 import userReducer from "./user-reducer";
 import appStateReducer from "./app-state-reducer";
+import postReducer from "./post-reducer";
 // create root reducer
 const rootReducer = combineReducers({
     user: userReducer,
     appState: appStateReducer,
-    formState: formReducer
+    formState: formReducer,
+    posts: postReducer
 });
 export default rootReducer;
