87775fb62b8d76101b36e062e20200a6318fcfcb
imasshaWe <imasha98.we@gmail.com>
2023-02-05 10:58:15 +0530

    update to verify email

--- packages/frontend/src/views/common/require-auth.js
+++ packages/frontend/src/views/common/require-auth.js
@@ -1,5 +1,5 @@
 import {Navigate, useLocation} from "react-router-dom";
-import {LOGIN_PATH} from "../../config/paths";
+import {EMAIL_NOT_VERIFY_PATH, LOGIN_PATH} from "../../config/paths";
 import {useSelector} from "react-redux";
 
 export default function RequireAuth({children}) {
@@ -7,6 +7,9 @@ export default function RequireAuth({children}) {
     const user = useSelector(state => state.user);
     if (!user) {
         return <Navigate to={LOGIN_PATH} replace={true} state={{redirect: location.pathname}}/>
+    } else if (user && !user.isVerified) {
+        return <Navigate to={EMAIL_NOT_VERIFY_PATH} replace={true}/>
+    } else {
+        return children;
     }
-    return children;
 }
