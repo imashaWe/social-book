554e20082624e8de80e8b7788076c9b94f5e9ce2
imasshaWe <imasha98.we@gmail.com>
2023-02-05 13:24:47 +0530

    remove verify email path from protection

--- packages/backend/src/api/middlewares/authentication.middleware.js
+++ packages/backend/src/api/middlewares/authentication.middleware.js
@@ -6,6 +6,6 @@ dotenv.config();
 const authentication = jwt({
     secret: process.env.ACCESS_TOKEN_SECRET,
     algorithms: ["HS256"],
-}).unless({path: ["/auth/login", '/auth/register']});
+}).unless({path: [/^\/auth\/.*/]});
 
 module.exports = authentication;
