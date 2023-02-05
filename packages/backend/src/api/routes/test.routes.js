cef860fe806aa65b9829910f76651d94dbc72518
imasshaWe <imasha98.we@gmail.com>
2023-02-06 01:20:49 +0530

    add mail send testing route

--- packages/backend/src/api/routes/test.routes.js
+++ packages/backend/src/api/routes/test.routes.js
@@ -1,10 +1,18 @@
 const express = require('express');
-
+const mailService = require('../../api/services/email.service');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    console.log(req.auth);
     res.send('Hello World!');
+});
+router.get('/sendmail', (req, res) => {
+    mailService.sendVerificationEmail('imasha98.we@gmail.com', 'Imasha', 'http://localhost:3000/verify/1234567890')
+        .then((result) => {
+            res.send(result);
+        })
+        .catch((err) => {
+            res.send(err);
+        });
 });
 
 module.exports = router;
