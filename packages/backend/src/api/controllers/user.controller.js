7db2b6d5d0e6b93a320e15d84fc4a03f1a5cb2d6
imasshaWe <imasha98.we@gmail.com>
2023-02-05 17:48:33 +0530

    add usernameOrEmail

--- packages/backend/src/api/controllers/user.controller.js
+++ packages/backend/src/api/controllers/user.controller.js
@@ -1,17 +1,17 @@
 const userServices = require('../services/user.service');
 const register = (req, res) => {
-    const {firstName, lastName, email, password} = req.body;
+    const {firstName, lastName, username, email, password} = req.body;
     userServices
-        .createUser(firstName, lastName, email, password)
+        .createUser(firstName, lastName, username, email, password)
         .then((data) =>
             res.status(200).json({status: true, message: 'User successfully registered', user: data}))
         .catch((e) => res.status(200).json({status: false, message: e.message}));
 }
 
 const login = (req, res) => {
-    const {email, password} = req.body;
+    const {usernameOrEmail, password} = req.body;
     userServices
-        .loginUser(email, password).then((data) =>
+        .loginUser(usernameOrEmail, password).then((data) =>
         res.status(200).json({status: true, message: 'User successfully logged in', user: data}))
         .catch((e) => res.status(200).json({status: false, message: e.message}));
 }
