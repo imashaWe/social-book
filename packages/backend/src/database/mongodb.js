4416e9193ab11079a55653f9cc28be78feb7fc71
imasshaWe <imasha98.we@gmail.com>
2023-02-06 07:58:31 +0530

    add exception handle to mongodb connect

--- packages/backend/src/database/mongodb.js
+++ packages/backend/src/database/mongodb.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
-const connect = () => {
-    mongoose.connect(process.env.MONGO_URL, {useNewUrlParser: true, useUnifiedTopology: true});
-    console.log("Connected to MongoDB");
+const connect = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB Connected...');
+    } catch (err) {
+        console.error("MongoDB connection error: ", err.message);
+        process.exit(1);
+    }
 }
 
 module.exports = {connect};
