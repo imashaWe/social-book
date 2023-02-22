505f7cdc7d1dc3db7a39aaa4b7c4d3f200cab8e3
imasshaWe <imasha98.we@gmail.com>
2023-02-22 19:35:36 +0530

    remove left ui components in auth layout

--- packages/frontend/src/views/layouts/auth-layout.js
+++ packages/frontend/src/views/layouts/auth-layout.js
@@ -1,64 +1,26 @@
-import {Grid, Stack, Typography} from "@mui/material";
-import surgeLogo from "../../assets/images/surge-logo.png";
-import IconButton from "@mui/material/IconButton";
-import {Facebook, GitHub, LinkedIn} from "@mui/icons-material";
+import {Grid, Box} from "@mui/material";
 import authCover from "../../assets/images/auth-cover.png";
 
 export default function AuthLayout({children}) {
     return (
-        <Grid container>
+        <Box
+            sx={{
+                backgroundImage: `url(${authCover})`,
+                backgroundSize: 'cover',
+                height: "100vh"
+            }}>
 
-            <Grid item
-                  xs={8}
-                  sx={{
-                      backgroundImage: `url(${authCover})`,
-                      backgroundSize: 'cover',
-                      height: "100vh"
-                  }}>
+            <Grid container
+                  justifyContent="center"
+                  alignItems="center"
+                  sx={{height: 1}}>
 
-                <Grid container
-                      justifyContent="center"
-                      alignItems="center"
-                      sx={{height: 1}}>
-
-                    <Grid item xs={6}>
-                        {children}
-                    </Grid>
-
+                <Grid item xs={4}>
+                    {children}
                 </Grid>
 
             </Grid>
 
-            <Grid item xs={4}>
-                <Grid container
-                      justifyContent="center"
-                      alignItems="center"
-                      sx={{height: 1}}>
-                    <Grid item xs={5}>
-
-                        <Stack justifyContent="center"
-                               alignItems="center"
-                               spacing={2}>
-
-                            <img src={surgeLogo} height={117} alt="surge-logo"/>
-
-                            <Typography variant='h5' align='center'>
-                                Software Engineering Internship March 2023
-                            </Typography>
-
-                            <Typography>Imasha Weerakoon</Typography>
-
-                            <Stack direction='row'>
-                                <IconButton><Facebook/></IconButton>
-                                <IconButton><LinkedIn/></IconButton>
-                                <IconButton><GitHub/></IconButton>
-                            </Stack>
-
-                        </Stack>
-                    </Grid>
-                </Grid>
-            </Grid>
-
-        </Grid>
+        </Box>
     );
 }
